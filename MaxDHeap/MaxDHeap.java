0dcdb36dc94afcd2986a6769509456f047e5dc0e
Oz <ozanani94@gmail.com>
2017-03-05 10:39:54 +0200

    Minor changes

--- MaxDHeap/MaxDHeap.java
+++ MaxDHeap/MaxDHeap.java
@@ -3,8 +3,8 @@
  * using d-ary maximum heap, to sort an array of integers.
  *
  * "A d-ary heap is like a binary heap, but (with one possible exception) non-leaf
- * nodes have d children instead of 2 children." (problem 4.2, "Introduction to Algorithms",
- * by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein.
+ * nodes have d children instead of 2 children." (problem 6.2, "Introduction to Algorithms",
+ * by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein)
  *
  * The heap is represented by an array.
  *
